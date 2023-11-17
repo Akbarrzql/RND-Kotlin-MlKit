5401732648322a886bc568d2b405af4aba55f54a
Akbar Rizqullah <akbarrizqullah228@gmai.com>
2023-11-17 10:35:22 +0700

    test camera x

---
+++ objectdetectionplate/src/main/java/com/example/objectdetectionplate/ObjectDetectionPlateActivity.kt
@@ -0,0 +1,11 @@
+package com.example.objectdetectionplate
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ObjectDetectionPlateActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_object_detection_plate)
+    }
+}
